df598031f8c2c351475dee2f1402d4258ac10ea8
aluno <aluno@etehort.ps.sp>
2022-02-22 20:28:07 -0300

    Calculadora

---
+++ Aula22-02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula22_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            operacao op = new operacao(); 
+            double soma, multiplicacao, divisao, subtracao;
+            Console.WriteLine("Digite o primeiro numero a ser calculado");
+
+            Console.WriteLine("Digite o primeiro numero a ser calculado");
+            op.n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero a ser calculado");
+            op.n2 = Convert.ToDouble(Console.ReadLine());
+
+            soma = op.n1 + op.n2;
+            subtracao = op.n1 - op.n2;
+            divisao = op.n1 / op.n2;
+            multiplicacao = op.n1 * op.n2;
+
+            Console.WriteLine("Soma = "+ soma);
+            Console.WriteLine("Subtração = " + subtracao);
+            Console.WriteLine("Multiplicação = " + multiplicacao);
+            Console.WriteLine("Divisão = " + string.Format("{0:0.00}",divisao));
+
+        }
+    }
+}
---
+++ Aula22-02/operacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula22_02
+{
+    class operacao
+    {
+        public double n1;
+        public double n2;
+    }
+}
